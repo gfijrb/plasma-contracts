b42161acfaed81f40c9287356f9c2019c11f00bc
Paul Peregud <paulperegud@gmail.com>
2019-01-03 15:06:32 +0100

    consistent naming of event fields

--- contracts/RootChain.sol
+++ contracts/RootChain.sol
@@ -123,7 +123,7 @@ contract RootChain {
     event InFlightExitOutputBlocked(
         address indexed challenger,
         bytes32 txHash,
-        uint256 outputId
+        uint256 outputIndex
     );
 
     event ExitFinalized(
